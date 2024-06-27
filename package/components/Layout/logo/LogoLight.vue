c95366d4b1ae8429b80a8265cec06f89475e03e0
Lukas2094 <lukas2_8@hotmail.com>
2024-06-27 14:54:08 -0300

    add dash

---
+++ package/components/Layout/logo/LogoLight.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+<template>
+    <div class="logo">
+      <NuxtLink to="/" class="d-flex">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-0iQZ13cBjbrZnO6pTwNbzUmK9unX2THBRw&s"  alt="logo"/>
+      </NuxtLink>
+    </div>
+  </template>
+
+  
---
+++ package/components/dashboard/TheActivityTimeline.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+<style lang="scss">
+.profiletimeline {
+  position: relative;
+  padding-left: 40px;
+  margin: 10px 10px 0 30px;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
+
+  .sl-item {
+    margin-top: 8px;
+    margin-bottom: 30px;
+    display: flex;
+  }
+
+  .sl-left {
+    margin-left: -60px;
+    z-index: 1;
+    margin-right: 15px;
+  }
+}
+</style>
